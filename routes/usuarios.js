516bc4f8d28dfd193f92ffa5bafd3b697520e3f8
Leonardo Kuffo <lkuffo@espol.edu.ec>
2017-02-18 17:12:23 -0500

    Modulo de Usuarios - Envio de Correo

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require("nodemailer")
 
 var Usuario = require('../models/usuario');
+var transport = nodemailer.createTransport('smtps://kufgal%40gmail.com:galoGALOkuffoKUFFO22@smtp.gmail.com');
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -39,6 +42,23 @@ router.post('/', function(req,res,next){
     usuario.save(function(err){
         if (err) return handleError(err);
     });
+
+    var message = {
+        from: 'fpsandbox@example.com',
+        to: req.body.correoUsuario,
+        subject: "BIENVENIDO! Fundamentos de Programacion",
+        text: "BIENVENIDO AL CURSO DE FUNDAMENTOS DE PROGRAMACION!\n\n Para poder acceder al Sandbox del curso utilice la siguiente contrasena: "+req.body.passwordUsuario,
+        html: '<h2>BIENVENIDO AL CURSO DE FUNDAMENTOS DE PROGRAMACION!</h2><br><br><p> Para poder acceder al Sandbox del curso utilice la siguiente contrasena: '+req.body.passwordUsuario+'</p>'
+    };
+
+    transport.sendMail(message, function(error){
+        if (error){
+            console.log('Error Ocurred');
+            console.log(error.message);
+            return;
+        }
+    })
+
     res.redirect("/users");
     
 });
